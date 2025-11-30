cd896ac56dc8b175e721da0024ce830329f3e2be
Marcos Peque Marban <159158821+MarcosPequeMarban@users.noreply.github.com>
2025-11-30 17:58:59 +0100

    Implement user entity and DAO

--- app/src/main/java/com/example/healthywallet/database/DAO/UsuarioDao.java
+++ app/src/main/java/com/example/healthywallet/database/DAO/UsuarioDao.java
@@ -1,4 +1,21 @@
 package com.example.healthywallet.database.DAO;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.healthywallet.database.entities.Usuario;
+
+@Dao
 public interface UsuarioDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long insertar(Usuario usuario);
+
+    @Query("SELECT * FROM usuarios LIMIT 1")
+    Usuario obtenerUnico();
+
+    @Query("DELETE FROM usuarios")
+    int eliminarTodos();
 }
